5bd9b601cff97e20d8e6e70a867f37c4efc80157
NowTempSomething <adsadsf1@gmail.com>
2022-04-07 20:35:58 +0900

    master searcher

--- src/scripts/searcher.java
+++ src/scripts/searcher.java
@@ -1,6 +1,5 @@
 package scripts;
 
-//feature의 searcher
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.ObjectInputStream;
@@ -14,10 +13,10 @@ import org.snu.ids.kkma.index.Keyword;
 import org.snu.ids.kkma.index.KeywordExtractor;
 import org.snu.ids.kkma.index.KeywordList;
 import org.w3c.dom.Document;
-import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
+//master의 searcher
 public class searcher {
 	@SuppressWarnings({"rawtypes", "unchecked", "nls"})
 	void CalcSim(String path, String indexPath, String words) {
@@ -43,86 +42,109 @@ public class searcher {
 				keyValue[i] = new double[splitKeyValue.length];
 				for(int a=0; a<splitKeyValue.length; a++) {
 					keyValue[i][a] = Double.parseDouble(splitKeyValue[a]);
-				}		
+					
+				}
 				i++;
 			}
-			
-			
-			
-			
-			
-			//받은 word값 kkma분석기 사용	
-			KeywordExtractor ke = new KeywordExtractor();
-			KeywordList kl = ke.extractKeyword(words, true);
-			
-			String wordsKey[] = new String[kl.size()];
-			int wordsValue[] = new int[kl.size()];
-			for(int x =0 ; x<kl.size(); x++) {
-				Keyword kwrd = kl.get(x);
-				wordsKey[x] = kwrd.getString();
-				wordsValue[x] = kwrd.getCnt();
-			}
-			
-			double[] result = new double[keyValue[0].length];
-			int[] resultIndex = new int[result.length];
-			for (int w = 0; w < result.length; w++) {
-				resultIndex[w] = w;
-			}
-			
-			
-			for(int j =0 ; j< wordsKey.length; j++) {
-				for(int l = 0; l < keyArr.length; l++) {
-					if(wordsKey[j].equals(keyArr[l])) {
-						for(int k = 0; k < keyValue[l].length; k++) {
-							result[k] += wordsValue[j]*keyValue[l][k];
+			//keyArr[i] = index.post i번쨰키워드
+			//keyValue[i][a] = i번쨰 키워드 id=a일떄 개수
+			//wordsKey[i] = words i번째 키워드
+			//wordsValue[i] = words i번째 개수
+				
+				//받은 word값 kkma분석기 사용	
+				KeywordExtractor ke = new KeywordExtractor();
+				KeywordList kl = ke.extractKeyword(words, true);
+				
+				String wordsKey[] = new String[kl.size()];
+				int wordsValue[] = new int[kl.size()];
+				for(int x =0 ; x<kl.size(); x++) {
+					Keyword kwrd = kl.get(x);
+					wordsKey[x] = kwrd.getString();
+					wordsValue[x] = kwrd.getCnt();
+				}
+				
+				//result = 내적값
+				double[] result = new double[keyValue[0].length];
+				int[] resultIndex = new int[result.length];
+				for (int w = 0; w < result.length; w++) {
+					resultIndex[w] = w;
+				}
+				
+				double[] Wroot = new double[result.length];
+				for(int aa = 0; aa < Wroot.length; aa++) {
+					Wroot[aa] = 0;
+				}
+				
+				
+				for(int j =0 ; j< wordsKey.length; j++) {
+					for(int l = 0; l < keyArr.length; l++) {
+						if(wordsKey[j].equals(keyArr[l])) {
+							for(int k = 0; k < keyValue[l].length; k++) {
+								result[k] += wordsValue[j]*keyValue[l][k];
+								Wroot[k] +=keyValue[l][k]*keyValue[l][k];
+							}
 						}
 					}
+					
 				}
-			}
-			
-			//가장 큰값이 낮은 숫자로 오도록 sort
-			for(int end = 0; end < result.length; end++) {
-				for(int end2 = end+1; end2 < result.length; end2++) {
-					if(result[end] < result[end2]) {
-						double temp = result[end];
-						result[end] = result[end2];
-						result[end2] = temp;
-						
-						int itemp = resultIndex[end];
-						resultIndex[end] = resultIndex[end2];
-						resultIndex[end2] = itemp;
+				//현재 내적값 result[k]에 보관되어있음.
+				
+				double[] Sim = new double[result.length];
+				double[] Qroot = new double[result.length];
+
+				
+				for(int t = 0; t < result.length; t++) {
+					Qroot[t] = 0;
+					for(int ss =0; ss< wordsKey.length; ss++) {
+						Qroot[t] += wordsValue[ss]*wordsValue[ss];
 					}
-				}							
-			}
-			
-			
-			//index.xml 읽어서 문서 번호 가져오기
-			DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
-			DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
-			File folder = new File(indexPath);
-			Document CheckID = docBuilder.parse(folder);
-			
-			
-			for(int e= 0; e < 3; e++) {
-				if(result[e] == 0) {
-					if(e == 0) {
-						System.out.println("입력한 값과 유사한 doc이 없습니다.");
+					
+					Qroot[t] = Math.sqrt(Qroot[t]);
+					Wroot[t] = Math.sqrt(Wroot[t]);
+					//Wroot가 0일경우 분모가 0 => sim값 0으로 취급함.
+					if(Wroot[t] == 0) Sim[t] = 0;
+					else {
+						Sim[t] = result[t] / (Qroot[t] * Wroot[t]);
 					}
-					else break;
+					
+					System.out.println(result[t] + " / ( " + Qroot[t] + " * " + Wroot[t] + ") = " + Sim[t]);
 				}
-				else {
-					NodeList titleList = CheckID.getElementsByTagName("title").item(resultIndex[e]).getChildNodes();
-					Node Title = (Node) titleList.item(0);
-					String titleData = Title.getNodeValue();
-					System.out.println(titleData);
+				//가장 큰값이 낮은 숫자로 오도록 sort
+				for(int end = 0; end < Sim.length; end++) {
+					for(int end2 = end+1; end2 < Sim.length; end2++) {
+						if(Sim[end] < Sim[end2]) {
+							double temp = Sim[end];
+							Sim[end] = Sim[end2];
+							Sim[end2] = temp;
+							
+							int itemp = resultIndex[end];
+							resultIndex[end] = resultIndex[end2];
+							resultIndex[end2] = itemp;
+						}
+					}							
 				}
+				//index.xml 읽어서 문서 번호 가져오기
+				DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+				DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+				File folder = new File(indexPath);
+				Document CheckID = docBuilder.parse(folder);
+				
+				for(int e= 0; e < 3; e++) {
+					if(Sim[e] == 0) {
+						if(e == 0) {
+							System.out.println("입력한 값과 유사한 doc이 없습니다."); break;
+						}
+						else break;
+					}
+					else {
+						NodeList titleList = CheckID.getElementsByTagName("title").item(resultIndex[e]).getChildNodes();
+						Node Title = (Node) titleList.item(0);
+						String titleData = Title.getNodeValue();
+						System.out.println(titleData);
+					}
+				}
+			}catch (Exception e) {
+				e.printStackTrace();
 			}
-			
-			
-			
-			
-		}catch (Exception e) {
-			e.printStackTrace();
-		}
 	}
 }
